c752b0b37c271258cae6590a3d5a2e74c701839c
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-14 22:18:13 -0400

    Agregando el Codigo de las dos Entidades, Moras y MorasDetalle.

--- Model/Moras.cs
+++ Model/Moras.cs
@@ -7,5 +7,20 @@ namespace Registro_de_Prestamos.Model
 {
     public class Moras
     {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Total { get; set; }
+
+        [ForeignKey("MoraId")]
+        public List<MorasDetalle> Detalle { get; set; }
+
+        public Moras()
+        {
+            MoraId = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            Detalle = new List<MorasDetalle>();
+        }
     }
 }
--- Model/MorasDetalle.cs
+++ Model/MorasDetalle.cs
@@ -7,6 +7,12 @@ namespace Registro_de_Prestamos.Model
 {
     public class MorasDetalle
     {
+        [Key]
+        public int Id { get; set; }
+        public int MoraId { get; set; }
+        public int PrestamoId { get; set; }
+        public string NombrePersona { get; set; }
+        public double Valor { get; set; }
     }
 }
  
