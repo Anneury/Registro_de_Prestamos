9a4470e601ce16fe4e7555db77d65522fa5c245a
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-14 22:26:38 -0400

    Eliminando cosas inutiles.

--- Startup.cs
+++ Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Registro_de_Prestamos.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +29,6 @@ namespace Registro_de_Prestamos
             services.AddBlazoredToast();
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
